1fff8e427d59960b5a2604587ae0e182d06d9a86
kakl <kakl@f55caa52-0715-0410-8694-db4083128028>
2016-05-26 20:17:57 +0000

    scripts was renamed

--- Designs/ROBOTS/IRRAD01A/SW/video.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-vlc v4l2:// :v4l2-dev=/dev/video0 :v4l2-width=640 :v4l2-height=480 --sout="#transcode{vcodec=MJPG,vb=800,scale=1,acodec=none}:http{mux=mpjpeg,dst=147.231.24.61:8080/}" -I dummy
